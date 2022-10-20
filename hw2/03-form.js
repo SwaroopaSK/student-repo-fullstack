d0b4a4e229a83a9f8fbde069c9e88ac3fdb94dcc
SwaroopaSK <91866875+SwaroopaSK@users.noreply.github.com>
2022-10-20 13:28:44 -0700

    Update 03-form.js

--- hw2/03-form.js
+++ hw2/03-form.js
@@ -1,3 +1,28 @@
 /** Exercise 03 - Form **/
 
-// Add your code here
+let form = document.querySelector('form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    const name = form.elements.name.value;
+    const email = form.elements.email.value;
+    let message = form.elements.message.value;
+	if (message == ""){
+	console.log('No feedback was submitted ' + message);
+	if (form.elements.signup.checked){
+	console.log('Yes, I would like to join the newsletter.');
+	}else{
+		console.log('No, thank you.');
+	}
+
+    console.group('================ Form Submission ==================')
+    console.log('Name: ' + name);
+    console.log('Email: ' + email);
+    console.log('Feedback: ' + message);
+    console.log('Newsletter: ' + signup);
+    console.groupEnd();
+
+    event.preventDefault();
+}
+}
