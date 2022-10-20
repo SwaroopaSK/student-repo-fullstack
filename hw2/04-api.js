839e006fdee26c4bb6198f146ce18643bc4ee577
SwaroopaSK <91866875+SwaroopaSK@users.noreply.github.com>
2022-10-20 13:30:45 -0700

    Update 04-api.js

--- hw2/04-api.js
+++ hw2/04-api.js
@@ -1,7 +1,20 @@
-/** Exercise 04 - API **/
+const url = 'https://restcountries.com/v2/all';
 
-const url = 'https://restcountries.com/v3.1/all';
+// Enter your code here
+function getpage(url) {
+    fetch(url).then(response => response.json()).
+        then(data => displayNames(data));
+}
 
-// Add your code here
+function displayNames(data){
+    var list = document.getElementById('results');
+    var newEntry =  null;
+    for(country of data){
+        newEntry = document.createElement('li');
+        newEntry.innerHTML=country.name+" - "+country.population.toLocaleString();
+        list.appendChild(newEntry);
 
-getData(url);
+    }
+}
+
+getpage(url);
