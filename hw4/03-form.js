4ee374b156a0ecf1f91ab4e41acc534f251e9522
SwaroopaSK <91866875+SwaroopaSK@users.noreply.github.com>
2022-11-13 19:13:21 -0800

    Update 03-form.js

--- hw4/03-form.js
+++ hw4/03-form.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
+var path = require('path');
 
 // Use middleware static() to serve all static files in the given folder
 app.use(express.static('public'));
@@ -8,11 +9,31 @@ app.use(express.static('public'));
 // Use middleware urlencoded() to parse an incoming request with a urlencoded payload and return an objectß
 app.use(express.urlencoded({ extended: false }));
 
-// POST request
-app.post('', (req, res) => {
-  // Add your code here
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname+'/index.html'));
+    res.end();
 });
 
+app.post('/submit', function (req, res) {
+    var name = "Name :" +" " +req.body.name;
+    var email =   "</br>" + "Email :" + req.body.email ;
+    var comments = req.body.comments;
+    if(comments == ""){
+      var commentsRes = "</br>" + "Comments : N/A";
+    }
+    else {
+    commentsRes = "</br>" + "Comments :" + comments ;
+    }    
+    var newsletter =  req.body.check;
+    if(newsletter == "on"){
+        var newsletterRes = "</br>" + "Newsletter :" + "Yes , sign me up for the newsletter.";
+    }
+    else {
+      newsletterRes = "</br>" + "Newsletter :" + "No, thank you."
+    }    
+    res.send(name + "</br>" + email + "</br>" + commentsRes + "</br>" + newsletterRes);
+    
+});
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
